c08c6ef9fe9f3ced57363312226c653d62ae169c
bagganikhil3223 <bagganikhil3223@gmail.com>
2019-05-23 11:27:57 +0530

    Add Project

---
+++ src/com/gym/data/ConnectionFactory.java
@@ -0,0 +1,14 @@
+package com.gym.data;
+import java.sql.Connection;
+import java.sql.DriverManager;
+	public class ConnectionFactory {
+
+		public static Connection getCon() throws Exception{
+			
+			Connection con=null;
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gym?allowPublicKeyRetrieval=true&useSSL=false","root","root");
+			return con;
+		}
+		
+}
